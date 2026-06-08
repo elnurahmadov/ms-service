dd701b7072a1105b440466db329409a59126b5e3
elnur.ahmadov <elnur.ahmadov@cybernet.az>
2026-06-08 15:19:28 +0400

    Rename liquibase files

--- src/main/java/az/ingress/service/model/constants/Cache.java
+++ src/main/java/az/ingress/service/model/constants/Cache.java
@@ -6,7 +6,7 @@ import static lombok.AccessLevel.PRIVATE;
 
 @NoArgsConstructor(access = PRIVATE)
 public final class Cache {
-    public static final int CACHE_EXPIRATION_HOURS = 24;
-    public static final String SERVICES_GROUP_CACHE_KEY = "ms-service::services_group";
-    public static final String SERVICES_CACHE_KEY = "ms-service::services";
+    public static final int CACHE_EXPIRATION_HOURS = 1;
+    public static final String SERVICES_GROUP_CACHE_KEY = "ms-service:services_group";
+    public static final String SERVICES_CACHE_KEY = "ms-service:services";
 }
